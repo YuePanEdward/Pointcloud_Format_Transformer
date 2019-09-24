213f6382bae142ecdeac9af8c9636ddcff4adf6e
Yue Pan (Edward) <2015301610150@whu.edu.cn>
2019-09-24 15:11:14 -0600

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-infolder=/media/edward/BackupPlus/Data/kitti-dataset/sequences/10;
-for file in ${infolder}/velodyne/*.bin
-do 
-	./bin/bin2pcd $file ${infolder}/HDL64/`basename $file .bin`.pcd
-done
